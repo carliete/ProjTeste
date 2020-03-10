a0249ae6916d6e38cb3aa0d2b2bddcb55d3f2556
carliete <49958085+carliete@users.noreply.github.com>
2020-03-10 15:20:01 -0300

    Update nos testes

--- teste1/src/test/java/teste1/PersonTestes.java
+++ teste1/src/test/java/teste1/PersonTestes.java
@@ -9,6 +9,7 @@ import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
@@ -24,6 +25,30 @@ class PersonTestes {
 	private String admin2 = "pass";
 	private String nome = "nome";
 	private String nova = "Site";
+	
+	private static boolean conexao;
+	
+	@Test
+	@BeforeAll
+	public static void testeBeforeAll() {
+		// Testar se a conneção com o banco de dados foi Aberta
+		conexao = true;
+		Assumptions.assumeTrue(conexao,"Conexao não iniciada");
+		System.out.println("Iniciei a conexÃ£o com o banco de dados");
+
+	}
+	
+	
+	@Test
+	@AfterAll
+	public static void testeAfterAll() {
+		// Testar se a conneção com o banco de dados foi fechada
+		conexao = false;
+		Assumptions.assumeFalse(conexao,"Conexao não fechou");
+		System.out.println("Fechou a conexÃ£o com o banco de dados");
+		
+		
+	}
 
 	@Test
 	@Disabled
@@ -55,45 +80,47 @@ class PersonTestes {
 	@Test
 	@Disabled
 	void testNotEquals() {
-		//Retorna erro se tiver duas paginas com o mesmo nome
+		// Retorna erro se tiver duas paginas com o mesmo nome
 		Pagina pagi = new Pagina("Blog", "a");
 		assertNotEquals(pagi.nome, nova, "Pagina Já existente");
 	}
-	
-	@Test 
+
 	@BeforeEach
 	@Disabled
 	void testeBefore() {
-		
+
 	}
-	
-	@Test 
+
 	@AfterEach
+	@Disabled
 	void testAfter() {
-		
-		
+
 	}
-	
-	
-	/*
-	 * @BeforeAll public static void testeBeforeAll() { // Testar se a conneção com
-	 * o banco de dados foi Aberta
-	 * System.out.println("Iniciei a conexÃ£o com o banco de dados"); }
-	 * 
-	 * @AfterAll public static void testeAfterAll() { // Testar se a conneção com o
-	 * banco de dados foi fechada
-	 * System.out.println("Fechou a conexÃ£o com o banco de dados"); }
-	 */
-	
-	@Test 
+
+	@Test
+	@Disabled
 	@Timeout(value = 600, unit = TimeUnit.MILLISECONDS)
 	void testeTime() throws InterruptedException {
-		//Se o salvamento passar do limite de tempo
-		//wait(700);
-		User user = new User (admin, admin2);
+		// Se o salvamento passar do limite de tempo
+		// wait(700);
+		User user = new User(admin, admin2);
 		System.out.println(user);
-		
+
 	}
 	
-	
+	@Test
+	@Disabled
+	public void AssumeFalse() {
+		//Se a condiçao nao for falsa da erro
+		Assumptions.assumeFalse(conexao);
+		System.out.println("Conexao AssumeFalse");
+	}
+	@Test
+	@Disabled
+	public void AssumeTrue() {
+		//Se a condição não for verdadeira da erro
+		Assumptions.assumeTrue(conexao);
+		System.out.println("Conexao AssumeTrue");
+	}
+
 }
